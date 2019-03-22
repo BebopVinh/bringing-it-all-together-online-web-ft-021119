20db7310d0db5211bff621dd65ecbcce766f075d
Vinh Do <vinhdo07@gmail.com>
2019-03-22 18:01:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,4 +63,9 @@ class Dog
   def self.new_from_db(row)
     Dog.new(id: row[0], name: row[1], breed: row[2])
   end
+
+  def self.find_by_name(name)
+    row = DB[:conn].execute("SELECT * FROM dogs WHERE name = (?)", name).flatten
+    new_from_db(row)
+  end
 end #end of class
